a24b35e6064a35429bdf3d519ac21f377589cd25
yasirelbouazzati <yasir12bouazzati@gmail.com>
2024-06-01 14:51:24 +0200

    complete

--- src/components/CreateNoteForm.jsx
+++ src/components/CreateNoteForm.jsx
@@ -3,6 +3,7 @@ import validateNonEmpty from "../helpers/validation/validateNonEmpty";
 import validateType from "../helpers/validation/validateType";
 import notesService from "../services/notes/notesService";
 import { useCreateForm } from "../hooks";
+import "./CreateNoteForm.css";
 
 export const CreateNoteForm = ({ note, setNote }) => {
   const {
@@ -41,68 +42,72 @@ export const CreateNoteForm = ({ note, setNote }) => {
 
   return (
     <div className="container">
-      <h2>Add a New Note:</h2>
-      <form onSubmit={addNewNote}>
-        <div className="form-group">
-          <label htmlFor="new-Name">Name:</label>
-          <input
-            id="new-Name"
-            type="text"
-            value={newName}
-            onChange={handleNewNameValue}
-            className="form-control"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="new-Description">Description:</label>
-          <input
-            id="new-Description"
-            type="text"
-            value={newDescription}
-            onChange={handleNewDescriptionValue}
-            className="form-control"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="new-Important">Important:</label>
-          <input
-            type="checkbox"
-            id="new-Important"
-            checked={newImportant}
-            onChange={handleNewImportantValue}
-            className="form-check-input"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="new-Status">Status (In Progress/Pending/Done):</label>
-          <input
-            id="new-Status"
-            type="text"
-            value={newStatus}
-            onChange={handleNewStatusValue}
-            className="form-control"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="new-Due_Date">Due Date (MM/DD/YYYY):</label>
-          <input
-            id="new-Due_Date"
-            type="date"
-            value={newDue_Date}
-            onChange={handleNewDue_DataValue}
-            className="form-control"
-            required
-          />
-        </div>
-        <div>
-          <button type="submit" className="btn btn-primary">
-            Add
-          </button>
-        </div>
-      </form>
+      <div className="form-wrapper">
+        <h2>Add a New Note:</h2>
+        <form onSubmit={addNewNote}>
+          <div className="form-group">
+            <label htmlFor="new-Name">Name:</label>
+            <input
+              id="new-Name"
+              type="text"
+              value={newName}
+              onChange={handleNewNameValue}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="new-Description">Description:</label>
+            <input
+              id="new-Description"
+              type="text"
+              value={newDescription}
+              onChange={handleNewDescriptionValue}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="new-Important">Important:</label>
+            <input
+              type="checkbox"
+              id="new-Important"
+              checked={newImportant}
+              onChange={handleNewImportantValue}
+              className="form-check-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="new-Status">
+              Status (In Progress/Pending/Done):
+            </label>
+            <input
+              id="new-Status"
+              type="text"
+              value={newStatus}
+              onChange={handleNewStatusValue}
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="new-Due_Date">Due Date (MM/DD/YYYY):</label>
+            <input
+              id="new-Due_Date"
+              type="date"
+              value={newDue_Date}
+              onChange={handleNewDue_DataValue}
+              className="form-control"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit" className="btn btn-primary">
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
